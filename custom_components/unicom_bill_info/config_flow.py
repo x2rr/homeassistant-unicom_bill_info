d3d54caad60f8211ee2109f9be1a73652a89a6f1
hlhk2017 <32770630+hlhk2017@users.noreply.github.com>
2025-04-30 20:30:24 +0800

    Update config_flow.py

--- custom_components/unicom_bill_info/config_flow.py
+++ custom_components/unicom_bill_info/config_flow.py
@@ -1,1 +1,22 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+from . import DOMAIN, CONF_OPENID, CONF_REFRESH_INTERVAL
 
+class UnicomBillInfoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            user_input[CONF_REFRESH_INTERVAL] = user_input[CONF_REFRESH_INTERVAL] * 60
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required(CONF_NAME, default="联通话费信息"): str,
+            vol.Required(CONF_OPENID): str,
+            vol.Required(CONF_REFRESH_INTERVAL, default=60): int  # 默认 60 分钟
+        })
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors
+        )
